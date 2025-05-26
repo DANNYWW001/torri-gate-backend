f2155dc572f7e2e9b32481471e7d0949c0051b12
DANNYWW001 <lekandaniel005@gmail.com>
2025-05-26 15:56:05 +0100

    feat: added property schema

---
+++ models/property.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const propertySchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    bedRoom: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    livingRoom: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    kitchen: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    toilet: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    paymentPeriod: {
+      type: String,
+      enum: ["yearly", "monthly", "weekly"],
+    },
+    images: [String],
+    availability: {
+      type: String,
+      enum: ["rented", "available"],
+      default: "available",
+    },
+    landlord: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+//MANUAL API documentation on how your API is to work
+const PROPERTY = mongoose.model("property", propertySchema);
+module.exports = PROPERTY;
